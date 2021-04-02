9d64af1dcf155b56507788baef88d6d1d8e139d5
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-04-02 14:00:08 -0500

    Added typeriter effect to homepage job title

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#main-nav'
-  })
+  target: '#main-nav'
+})
+
+$(document).ready(function () {
+
+  function typewriter() {
+    setTimeout(function () {
+
+      $("#job-title").attr("class", "text-center");
+
+    }, 6000)
+  }
+
+  typewriter();
+
+})
